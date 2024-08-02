71466db55f76c49988aa75a536aab823d38085ba
DKamil12 <dkamil381@gmail.com>
2024-08-02 16:33:35 +0600

    Added search field

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import Task
+from django import forms
 
 class TaskModelFrom(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'status']
+
+
+class TitleFilterForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        label='Search task by title'
+    )
--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,30 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
+from .filters import TaskFilter
+from .forms import TitleFilterForm
 
 
 class TaskListView(ListView):
     model = Task
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # self.filterset = TaskFilter(request=self.request.GET, queryset=qs)
+        # return self.filterset.qs
+
+        searched_title = self.request.GET.get('title')
+        if searched_title:
+            qs = qs.filter(title__contains=searched_title)
+        return qs
+    
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        # context['search_form'] = self.filterset.form
+        context['search_form'] = TitleFilterForm()
+        return context
 
 
 class TaskDetailView(DetailView):
